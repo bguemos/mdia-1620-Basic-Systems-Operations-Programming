f79c46b150ea711b1d4eee480304464aa6f4e3c6
Bri-lynn Guemos <tguemos@my.bcit.ca>
2022-09-16 12:15:34 -0700

    adding more code

--- part1/week2/test.js
+++ part1/week2/test.js
@@ -19,4 +19,22 @@ var i = 0;
 while(proceed) {
   i++; // i = 1;
   proceed = false;
-}
+};
+console.log(i);
+/**
+*6-4-1 Create a variable called grannySmith and add the following values (use camelCase)
+*colour of the apple is green
+*origin location is austrailia
+*orgin date is 1868
+*then console log out the colour from
+the object.
+*/
+var grannySmith= {
+    color:"green",
+    originlocation: "australia",
+    originDate: 1868
+
+};
+console.log(grannySmith);
+
+
